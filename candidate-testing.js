7ba65e705417eccdcd7fc54b37fd2f004d8829a8
bsbrake <>
2021-06-07 22:16:05 +0000

    Best attempt

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,31 +3,38 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = (" ");
+let candidateName = ("")
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride";
-let candidateAnswer = ("");
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question = ("Who was the first American woman in space? ");
+let correctAnswer = "Sally Ride ";
+let candidateAnswer = (" ");
+let questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
-function askForName(candidateName) {
-  let name = input.question("What is your name: ");
-  console.log("Welcome," + name +  "!");
-  
+function askForName() {
+candidateName = input.question("Enter your name: ");
+
+console.log("Welcome " + candidateName + "!");
 }
+
+
 
 function askQuestion() {
-  let info = input.question ("1. Who was the first American woman in space? ");
-  
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+  for (let i = 0; i < 5; i++)
+  console.log(questions[i]);
 }
 
-function gradeQuiz() {(candidateAnswers) 
+function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  console.log(` Your Answer: $(candidateAnswers)\n Correct Answer: $(correctAnswers)`);
   
+  
+  //console.log("Your Answer: " + candidateAnswer);
+  //console.log("Correct Answer: " + correctAnswer);
 
   let grade;
   
@@ -37,6 +44,10 @@ function gradeQuiz() {(candidateAnswers)
 
 function runProgram() {
   askForName();
+  // TODO 1.1c: Ask for candidate's name //
+
+  
+  
 
   askQuestion();
   gradeQuiz(this.candidateAnswers);
